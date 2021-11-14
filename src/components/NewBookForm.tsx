8d6998bc9e27c76f016655027d8bd2ba0a780fb2
MariSena331 <mariana7007@hotmail.com>
2021-11-14 07:44:09 -0300

    Centralizando formulário

--- src/components/NewBookForm.tsx
+++ src/components/NewBookForm.tsx
@@ -1,4 +1,4 @@
-import { Input, MenuItem, Modal, TextField } from '@material-ui/core'
+import { Card, Input, makeStyles, MenuItem, Modal, TextField } from '@material-ui/core'
 import { Form } from '@unform/web'
 import { FormHandles } from '@unform/core'
 import { useRef, useState } from 'react'
@@ -9,37 +9,53 @@ interface IMyBooksProps {
     handleClose: () => void
 }
 
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  card: {
+    minWidth: '44rem',
+  },
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+  }
+})
+
 export const NewBookForm = ({ open, handleClose }: IMyBooksProps) => {
     const formRef = useRef<FormHandles>(null)
+    const classes = useStyles()
     const [status, setStatus] = useState([' WANT_READ', 'READING', 'AlREADY_READ'])
 
-    const handleSubmit = useCallback(() => {
-
-    }, [])
+    const handleSubmit = useCallback(() => {}, [])
 
     return (
         <Modal
           open={open}
           onClose={handleClose}
-          aria-labelledby="simple-modal-title"
-          aria-describedby="simple-modal-description"
+          className={classes.root}
         >
-            <form onSubmit={handleSubmit}>
-                <Input name="name" aria-label="Titulo" />
-                <Input name="email" aria-label="Autor" />
-                <TextField
-                  id="filled-select-currency"
-                  select
-                  label="Status"
-                  variant="filled"
-                >
-                  {status.map((option, index) => (
-                    <MenuItem key={index} value={option}>
-                      {option}
-                    </MenuItem>
-                  ))}
-                </TextField>
-            </form>
+            <Card className={classes.card}>
+                <form onSubmit={handleSubmit} className={classes.form}>
+                    <Input name="name" aria-label="Titulo" />
+                    <Input name="email" aria-label="Autor" />
+                    <TextField
+                      id="filled-select-currency"
+                      select
+                      label="Status"
+                      variant="filled"
+                    >
+                      {status.map((option, index) => (
+                        <MenuItem key={index} value={option}>
+                          {option}
+                        </MenuItem>
+                      ))}
+                    </TextField>
+                </form>
+            </Card>
         </Modal>
     )
 }
